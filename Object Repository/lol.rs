88a4431ae750840924533b2e0fe45e3230629223
monris92 <42492566+monris92@users.noreply.github.com>
2019-11-20 14:09:55 +0700

    Notes

--- Object Repository/lol.rs
+++ Object Repository/lol.rs
@@ -4,6 +4,16 @@
    <name>lol</name>
    <tag></tag>
    <elementGuidId>8134d6bc-11e5-4591-ae5d-4d06e72a87fc</elementGuidId>
-   <selectorMethod>BASIC</selectorMethod>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>empty</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
 </WebElementEntity>
--- Scripts/test/Script1574169393035.groovy
+++ Scripts/test/Script1574169393035.groovy
@@ -13,3 +13,7 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
+WebUI.comment('include>feature>Login.feature')
+
+WebUI.comment('script>groovy>default>LoginSteps.groovy')
+
